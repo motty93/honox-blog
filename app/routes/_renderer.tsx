77663cf3c312f0e0f99e54c962a9d34005f0ffa5
motty93 <rdwbocungelt5@gmail.com>
2025-04-23 21:22:39 +0900

    fix: tailwindcss読み込み修正

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -8,8 +8,8 @@ export default jsxRenderer(({ children }) => {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
         <Link href="/app/style.css" rel="stylesheet" />
+        <link rel="icon" href="/favicon.ico" />
         <Script src="/app/client.ts" async />
       </head>
       <body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  content: ['./app/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
